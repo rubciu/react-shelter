b7115c693b338412b17341dafca706ef1c08caa3
Ruben Montes <ruben.montes.mendoza@gmail.com>
2021-05-25 18:51:44 +0200

    sign up form component and tests

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Card } from './Card/Card';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as DogList } from './DogList/DogList';
 export { default as AddDogForm } from './AddDogForm/AddDogForm';
+export { default as SignupForm } from './SignupForm/SignupForm';
