d3f5069558d3707915e473a5bc211e7baa657209
Ruben M <ruben.montes.mendoza@gmail.com>
2021-06-30 19:27:24 +0200

    test lazy

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,35 +1,38 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { lazy, Suspense } from "react";
+import { Switch, Route } from "react-router-dom";
 
-import { Navbar, DogList, AddDogForm, ProtectedRoute } from '..';
-import LoginPage from '../../pages/LoginPage/LoginPage';
-import SignUpPage from '../../pages/SignUpPage/SignUpPage';
-import styles from './App.module.scss';
+import { Navbar, DogList, AddDogForm, ProtectedRoute } from "..";
+import styles from "./App.module.scss";
+
+const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
+const SignUpPage = lazy(() => import("../../pages/SignUpPage/SignUpPage"));
 
 const App = (): JSX.Element => {
   return (
     <div className={styles.App}>
       <Navbar />
       <div className={styles.main}>
-        <Switch>
-          <ProtectedRoute path='/dogs/add'>
-            <AddDogForm />
-          </ProtectedRoute>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <ProtectedRoute path="/dogs/add">
+              <AddDogForm />
+            </ProtectedRoute>
 
-          <ProtectedRoute path='/dogs'>
-            <DogList />
-          </ProtectedRoute>
+            <ProtectedRoute path="/dogs">
+              <DogList />
+            </ProtectedRoute>
 
-          <Route path='/login'>
-            <LoginPage />
-          </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
 
-          <Route path='/signup'>
-            <SignUpPage />
-          </Route>
+            <Route path="/signup">
+              <SignUpPage />
+            </Route>
 
-          <Route exact path='/' component={() => <div>Home</div>}></Route>
-        </Switch>
+            <Route exact path="/" component={() => <div>Home</div>}></Route>
+          </Switch>
+        </Suspense>
       </div>
     </div>
   );
